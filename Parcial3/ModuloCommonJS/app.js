bbf3947c39deb9a94e47992bd62427db7b977266
JordanDiaz01 <jordandelangel.d@gmail.com>
2022-11-09 18:35:56 -0600

    notas

--- Parcial3/ModuloCommonJS/app.js
+++ Parcial3/ModuloCommonJS/app.js
@@ -1,4 +1,7 @@
 const modulo= require('./modulo.js')
 const colors= require('colors') //colors.js es un paquete que permite ponerle colores a la consola
 
+
+
+// no debe correrse con ayuda de la extensión code runner porque bugea el paquete de colors y aparecen numeros raros
 console.log(colors.rainbow(modulo.cuadrado(4).toString()))
