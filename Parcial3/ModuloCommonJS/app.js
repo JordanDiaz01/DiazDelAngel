83ada7dc03d2c69169d4a3b13eedf8d62f74820e
JordanDiaz01 <jordandelangel.d@gmail.com>
2022-11-09 18:33:33 -0600

    node modules

--- Parcial3/ModuloCommonJS/app.js
+++ Parcial3/ModuloCommonJS/app.js
@@ -1,3 +1,4 @@
 const modulo= require('./modulo.js')
+const colors= require('colors') //colors.js es un paquete que permite ponerle colores a la consola
 
-console.log(modulo.cuadrado(4))
+console.log(colors.rainbow(modulo.cuadrado(4).toString()))
