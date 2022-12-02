34083bd61f68dfbc476f4fd3188288044170061b
JordanDiaz01 <jordandelangel.d@gmail.com>
2022-12-02 08:00:11 -0600

    pdf

--- Parcial3/pdf/crearpdf.php
+++ Parcial3/pdf/crearpdf.php
@@ -1,10 +1,12 @@
 <?php
+$nombre=$_POST['nombre'];
 require('./FPDF/fpdf.php');
+
 
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'¡Hola, Mundo!');
+$pdf->Cell(40,10,'¡Hola, Mundo!'.$nombre);
 $pdf->Output();
 ?>
 
